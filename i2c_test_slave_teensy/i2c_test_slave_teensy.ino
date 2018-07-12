69d0d985ff34adc406a465a20f0e2a6df5ac79bb
minhv17 <minhv17@vt.edu>
2018-07-12 19:19:31 -0400

    Enabled Teensy to read analog values (position and torque raw data)

--- i2c_test_slave_teensy/i2c_test_slave_teensy.ino
+++ i2c_test_slave_teensy/i2c_test_slave_teensy.ino
@@ -1,16 +1,20 @@
 // Test sketch for slave device for I2C communication
-// Examples by Nicholas Zambetti used as reference
-// <https://www.arduino.cc/en/Tutorial/MasterWriter>
-// <https://www.arduino.cc/en/Tutorial/MasterReader>
+// Example sketches used for reference:
+//   I2C examples by Nicholas Zambetti
+//    <https://www.arduino.cc/en/Tutorial/MasterWriter>
+//    <https://www.arduino.cc/en/Tutorial/MasterReader>
+//   ADC examples for using Teensy ADC library by pedvide
+//    <https://github.com/pedvide/ADC/blob/master/examples/analogRead/analogRead.ino>
+//    <https://github.com/pedvide/ADC/blob/master/examples/readPin/readPin.ino>
 
-//#include <ADC.h>
+#include <ADC.h>
 #include <Wire.h>
 #include <Servo.h>
 
 // Adjust the I2C ID as needed
 const byte moduleID = 16;
 
-// Servo object and values
+// Servo
 const byte servoPin = 9;
 Servo servo;
 byte rxInt;
@@ -18,34 +22,55 @@ byte rxDec;
 float angle = 90.0;
 int mappedAngle = 1500;
 
-// Analog samples
-//const byte posPin = A2;
-//const byte torqPin = A9;
-uint16_t rawPosVal = 5500;
-uint16_t rawTorqVal = 18000;
-byte tx1 = (rawPosVal & 0xFF);
-byte tx2 = (rawPosVal >> 8);
-byte tx3 = (rawTorqVal & 0xFF);
-byte tx4 = (rawTorqVal >> 8);
+// Analog sampling
+ADC *adc = new ADC();
+const byte posPin = A2;  // Corresponds to ADC0
+const byte torqPin = A9; // Corresponds to ADC1
+uint16_t rawPosVal;
+uint16_t rawTorqVal;
+byte tx1;
+byte tx2;
+byte tx3;
+byte tx4;
 
-void setup() {
-  Serial.begin(9600);
-  //pinMode(posPin, INPUT);
-  //pinMode(torqPin, INPUT);
-  
+void setup()
+{
+  Serial.begin(9600); // For debugging
+  // Analog pins
+  pinMode(posPin, INPUT);
+  pinMode(torqPin, INPUT);
+
+  // ADC0
+  adc->setAveraging(16, ADC_0);
+  adc->setResolution(16, ADC_0);
+  adc->setConversionSpeed(ADC_CONVERSION_SPEED::LOW_SPEED, ADC_0);
+  adc->setSamplingSpeed(ADC_SAMPLING_SPEED::MED_SPEED, ADC_0);
+
+  // ADC1
+  adc->setAveraging(16, ADC_1);
+  adc->setResolution(16, ADC_1);
+  adc->setConversionSpeed(ADC_CONVERSION_SPEED::LOW_SPEED, ADC_1);
+  adc->setSamplingSpeed(ADC_SAMPLING_SPEED::MED_SPEED, ADC_1);
+
+  // I2C communication
   Wire.begin(moduleID);
   Wire.onRequest(sendValues);
   Wire.onReceive(receiveAngles);
-  
+
+  // Servo
   servo.attach(servoPin);
 
+  // Turn on built-in LED so we know the Teensy is on and the setup completed
   pinMode(13, OUTPUT);
   digitalWrite(13, HIGH);
 }
 
 void loop()
 {
-  //rawPosVal = analogRead(
+  rawPosVal = adc->adc0->analogRead(posPin);
+  rawTorqVal = adc->adc1->analogRead(torqPin);
+  
+  //delay(100);
 }
 
 // Function executed upon receiving angle from master device
@@ -61,12 +86,16 @@ void receiveAngles(int num)
   Serial.println(mappedAngle);
   
   servo.writeMicroseconds(mappedAngle);
-  
-  //delay(15);
 }
 
+// Function executed upon master device requesting to read values
 void sendValues()
 {
+  tx1 = (rawPosVal & 0xFF);
+  tx2 = (rawPosVal >> 8);
+  tx3 = (rawTorqVal & 0xFF);
+  tx4 = (rawTorqVal >> 8);
+  
   Wire.write(tx1);
   Wire.write(tx2);
   Wire.write(tx3);
