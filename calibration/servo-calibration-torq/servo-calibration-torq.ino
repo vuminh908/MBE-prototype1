6c14bc2d7410b45dee5e14fe296f5bae526042fa
malex <malex2@vt.edu>
2018-12-13 18:57:24 -0500

    SerialTorqueCalibration can save live ADC value
    SerialTorqueCalibration can save live ADC value and lets the user use the front panel every iteration. Saving still needs some debugging

--- calibration/servo-calibration-torq/servo-calibration-torq.ino
+++ calibration/servo-calibration-torq/servo-calibration-torq.ino
@@ -7,9 +7,10 @@
 Servo servo;
 const byte servoPin = 9;
 const byte angle = 90; // Keep servo at constant angle of 90 degrees
-int   anglePWM = 1558; // Keep servo at constant angle of 90 degrees
+int   anglePWM = 1500; // Keep servo at constant angle of 90 degrees
 // Servo 1: anglePWM = 1545;
 // Servo 2: anglePWM = 1580;
+// Servo 3: anglePWM = 1480;
 ADC *adc = new ADC();
 const byte adcPin = A2;
 uint16_t adcReading; // In this case, raw torque value
